a0bd9355f2d4345c022e96319c0b27aa17207d3a
Tasneem maqableh <103468147+engTasneemmaq@users.noreply.github.com>
2022-05-23 22:19:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import Cards from "./components/Cards";
-
 function App() {
-  return (
-    <div className="App">
-     <h1>Memory Game</h1>
-     <Cards/>
-    
-     
-    </div>
-  );
+    return (
+        <div className="App">
+            <div class="wrapper">
+                <span>M</span>
+                <span>E</span>
+                <span>M</span>
+                <span>O</span>
+                <span>R</span>
+                <span>Y</span>
+                <span> </span>
+                <span> </span>
+                <span>G</span>
+                <span>A</span>
+                <span>M</span>
+                <span>E</span>
+            </div>
+            <Cards/>
+        </div>
+    );
 }
-
 export default App;
